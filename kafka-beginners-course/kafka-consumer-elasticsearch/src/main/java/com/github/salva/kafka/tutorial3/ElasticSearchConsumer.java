1c4019e280b844180693b8651b927441928b73b6
Salvabb81 <salvabb81@users.noreply.github.com>
2020-11-02 18:51:29 +0100

    Elasticsearch Consumer. Manual Commit of Offsets

--- kafka-beginners-course/kafka-consumer-elasticsearch/src/main/java/com/github/salva/kafka/tutorial3/ElasticSearchConsumer.java
+++ kafka-beginners-course/kafka-consumer-elasticsearch/src/main/java/com/github/salva/kafka/tutorial3/ElasticSearchConsumer.java
@@ -42,6 +42,7 @@ public class ElasticSearchConsumer {
 		while (true) {
 			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
+			logger.info("Received " + records.count() + " records.");
 			for (ConsumerRecord<String, String> record : records) {
 
 				// 2 strategies to create IDs
@@ -59,10 +60,19 @@ public class ElasticSearchConsumer {
 
 				logger.info(indexResponse.getId());
 				try {
-					Thread.sleep(1000); // we introduce a small delay
+					Thread.sleep(10); // we introduce a small delay
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
+			}
+			logger.info("Committing offsets...");
+			consumer.commitSync();
+			logger.info("Offsets have been commmited...");
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 
@@ -81,6 +91,8 @@ public class ElasticSearchConsumer {
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+		properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
 
 		// create consumer
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
